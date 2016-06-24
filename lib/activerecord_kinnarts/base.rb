ab839c06a13b01c3d57b747630a966c34e686315
Anton Zhirkov <an.zhirkov@gmail.com>
2016-06-24 11:26:35 +0300

    create db table with csv file

--- lib/activerecord_kinnarts/base.rb
+++ lib/activerecord_kinnarts/base.rb
@@ -1,12 +1,22 @@
+require "csv"
+
 module ActiverecordKinnarts
   class Base
     def self.new
       puts 'new'
     end
-    def self.save
+
+    def save
       puts 'save'
     end
-    def self.update
+
+    def update
+    end
+
+    def self.create_db_table_with_fields(*params)
+      CSV.open("#{self.name}.csv", "wb") do |csv|
+        csv << params
+      end
     end
   end
 end
